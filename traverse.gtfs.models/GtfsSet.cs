3b82ef0e96049b4ba0c8f1239fbccf8125689e0b
Josh Rolstad <jrolstad@gmail.com>
2013-10-10 09:59:12 -0700

    Adding model to identify a set of GTFS data

---
+++ traverse.gtfs.models/GtfsSet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace traverse.gtfs.models
+{
+    public class GtfsSet
+    {
+        public List<Agency> Agencies { get; set; }  
+
+        public List<Calendar> Calendars { get; set; }  
+
+        public List<CalendarDate> CalendarDates { get; set; }  
+
+        public List<FareAttribute> FareAttributes { get; set; }  
+
+        public List<FareRule> FareRules { get; set; }  
+
+        public List<FeedInfo> FeedInfo { get; set; }  
+
+        public List<Frequency> Frequencies { get; set; }  
+
+        public List<Route> Routes { get; set; }  
+
+        public List<Shape> Shapes { get; set; }  
+
+        public List<Stop> Stops { get; set; }  
+
+        public List<StopTime> StopTimes { get; set; }  
+
+        public List<Transfer> Transfers { get; set; }  
+
+        public List<Trip> Trips { get; set; }  
+    }
+}
