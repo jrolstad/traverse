bcdaa8c82541b6810fe616eba56ec547b06e916d
Josh Rolstad <jrolstad@gmail.com>
2013-10-10 21:21:33 -0700

    Implementing trip ClassMap

--- traverse.domain.services/gtfs/class_maps/TripClassMap.cs
+++ traverse.domain.services/gtfs/class_maps/TripClassMap.cs
@@ -7,7 +7,15 @@ namespace traverse.domain.services.gtfs.class_maps
     {
         public override void CreateMap()
         {
-            
+            Map(t => t.BlockId).Name("block_id");
+            Map(t => t.DirectionId).Name("direction_id");
+            Map(t => t.RouteId).Name("route_id");
+            Map(t => t.ServiceId).Name("service_id");
+            Map(t => t.ShapeId).Name("shape_id");
+            Map(t => t.TripHeadsign).Name("trip_headsign");
+            Map(t => t.TripId).Name("trip_id");
+            Map(t => t.TripShortName).Name("trip_short_name");
+            Map(t => t.WheelchairAccessible).Name("wheelchair_accessible");
         }
     }
 }
