bad2e0bda98ecb4954b17f012377185c494e3134
Josh Rolstad <jrolstad@gmail.com>
2013-10-10 21:40:19 -0700

    Adding one bus away models

---
+++ traverse.onebusaway.models/OneBusAwayResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace traverse.onebusaway.models
+{
+    public class OneBusAwayResponse<T>
+    {
+        public string Version { get; set; }
+
+        public int? Code { get; set; }
+
+        public DateTime CurrentTime { get; set; }
+
+        public string Text { get; set; }
+
+        public T Data { get; set; }
+}
+}
