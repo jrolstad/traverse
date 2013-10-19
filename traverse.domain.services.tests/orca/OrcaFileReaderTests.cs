22e5cd63adcaed9a6cd78e210a321ef0d3708150
Josh Rolstad <jrolstad@gmail.com>
2013-10-18 20:02:18 -0700

    Implementing reading of sample data file

---
+++ traverse.domain.services.tests/orca/OrcaFileReaderTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using CsvHelper;
+using NUnit.Framework;
+using traverse.domain.services.io;
+using traverse.domain.services.orca;
+using traverse.orca.models;
+
+namespace traverse.domain.services.tests.orca
+{
+    [TestFixture]
+    public class OrcaFileReaderTests
+    {
+        private List<OrcaTransaction> _result;
+
+        [TestFixtureSetUp]
+        public void BeforeAll()
+        {
+            // Arrange
+            var zipFile = GetSampleTransactionFile();
+
+            var reader = new OrcaFileReader(new FileSystem(), new CsvFactory());
+
+            // Act
+            _result = reader.Read(zipFile);
+        }
+
+
+        [Test]
+        public void Then_each_transaction_is_read()
+        {
+            // Assert
+            Assert.That(_result.Count,Is.EqualTo(1143));
+        }
+
+        [Test]
+        public void Then_the_dates_are_read()
+        {
+            // Assert
+            Assert.IsTrue(_result.All(r=>r.Date>DateTime.MinValue));
+        }
+
+        [Test]
+        public void Then_the_item_is_read()
+        {
+            // Assert
+            Assert.IsTrue(_result.All(r => !string.IsNullOrWhiteSpace(r.Item)));
+        }
+
+        [Test]
+        public void Then_the_location_is_read()
+        {
+            // Assert
+            Assert.IsTrue(_result.All(r => !string.IsNullOrWhiteSpace(r.Location)));
+        }
+
+        [Test]
+        public void Then_the_product_is_read()
+        {
+            // Assert
+            Assert.IsTrue(_result.All(r => !string.IsNullOrWhiteSpace(r.Product)));
+        }
+
+
+        private Stream GetSampleTransactionFile()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            const string resourceName = "traverse.domain.services.tests.resources.orca_transactions.txt";
+
+            var stream = assembly.GetManifestResourceStream(resourceName);
+
+            return stream;
+        }
+
+    }
+}
---
+++ traverse.domain.services/orca/OrcaFileReader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using traverse.domain.services.io;
+using traverse.orca.models;
+
+namespace traverse.domain.services.orca
+{
+    public class OrcaFileReader
+    {
+        private readonly IFileSystem _fileSystem;
+        private readonly ICsvFactory _csvFactory;
+
+        public OrcaFileReader(IFileSystem fileSystem, ICsvFactory csvFactory)
+        {
+            _fileSystem = fileSystem;
+            _csvFactory = csvFactory;
+        }
+
+        public List<OrcaTransaction> Read(string filePath)
+        {
+            var file = _fileSystem.ReadFile(filePath);
+
+            return Read(file);
+        }
+
+        public List<OrcaTransaction> Read(Stream fileStream)
+        {
+            var configuration = CreateFileReaderConfiguration();
+
+            using (var textReader = new StreamReader(fileStream))
+            using (var reader = _csvFactory.CreateReader(textReader, configuration))
+            {
+                var data = reader.GetRecords<OrcaTransaction>();
+
+                return data.ToList();
+            }
+        }
+        private static CsvConfiguration CreateFileReaderConfiguration()
+        {
+            var configuration = new CsvConfiguration();
+            configuration.WillThrowOnMissingField = false;
+            configuration.SkipEmptyRecords = true;
+            configuration.Delimiter = "\t";
+
+            return configuration;
+        }
+    }
+}
