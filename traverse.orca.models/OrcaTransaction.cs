b32a68fd89151730af0d053dc401855c09772cc6
Josh Rolstad <jrolstad@gmail.com>
2013-10-18 19:47:38 -0700

    Adding models project for ORCA data

---
+++ traverse.orca.models/OrcaTransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace traverse.orca.models
+{
+    public class OrcaTransaction
+    {
+        public DateTime Date { get; set; }
+
+        public string Item { get; set; }
+
+        public string Location { get; set; }
+
+        public string Product { get; set; }
+    }
+}
